75586c216e14904219b885cc3778cf14959858b4
Jardelsr <sribeirodel@gmail.com>
2024-06-25 08:19:08 -0300

    ajustando dependencia do servidor

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 
@@ -25,8 +26,8 @@ app.get('/', (req, res) => {
 // Rotas da API
 app.use('/api/products', productRoutes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
